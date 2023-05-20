d5570f384ec6e355d6025fca5ebc72210f239d8b
Tushar-Pharswan <pharswantushar@gmail.com>
2023-05-20 13:41:50 +0530

    first commit

---
+++ javaBasic/basic.java
@@ -0,0 +1,19 @@
+package javaBasic;
+
+public class basic {
+    public static void main(String[] args) {
+        int a=2;
+        long new_1 = a; 
+        System.out.println(new_1);
+        double new_2 =a;
+        System.out.println(new_2);
+        float new_3 =a;
+        System.out.println(new_3);
+
+
+
+
+        
+    }
+    
+}

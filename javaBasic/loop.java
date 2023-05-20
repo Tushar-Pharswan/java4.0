0051813ead00ff029bf2b840c9edc66d8bbd41f3
Tushar-Pharswan <pharswantushar@gmail.com>
2023-05-20 15:12:10 +0530

    +

---
+++ javaBasic/loop.java
@@ -0,0 +1,42 @@
+package javaBasic;
+
+public class loop 
+{
+    public static void main(String[] args) 
+    {
+        // int i;
+        // for (i=0; i<5 ; i++)
+        // {
+        // System.out.println(i);
+        // }
+        
+        // int j=10;
+        // if(j > 20)
+        // {
+        //     System.out.println("greater");
+        // }
+        // else
+        // {
+        //     System.out.println("smaller");
+        // }
+
+      
+        // int ev; 
+        // for (ev=0; ev<=10; ev++)
+        // {
+        //     if(ev %2==0){
+        //     System.out.println(ev);
+        //     }
+        // }
+        
+
+        int table;
+        int c=2;
+        int z;
+        for (table=1; table<=10; table++){
+            z = c*table;
+            System.out.println(" 2 * " +table +" = " +z);
+        }
+    }
+
+}

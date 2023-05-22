a77bd4b0b8119c82cd700766e04de808320da04c
Tushar-Pharswan <119163815+Tushar-Pharswan@users.noreply.github.com>
2023-05-22 11:27:05 +0000

    ggg

---
+++ loops/multiplier_vertical.java
@@ -0,0 +1,17 @@
+package loops;
+
+public class multiplier_vertical {
+    public static void main(String[] args) {
+
+        for(int i=1;i<=10;i++){
+            
+            int z;
+
+            for(z=1;z<=10;z++)
+            System.out.print(  "  " +i+ " * " + z + " = " + i*z );
+            System.out.println();
+        }
+
+    }
+    
+}

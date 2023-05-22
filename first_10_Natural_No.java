d3157f5f5e5980fb8688888260ee423839020125
Tushar-Pharswan <pharswantushar@gmail.com>
2023-05-22 13:04:34 +0530

    done

---
+++ first_10_Natural_No.java
@@ -0,0 +1,10 @@
+public class first_10_Natural_No {
+    public static void main(String[] args) {
+        System.out.println("First 10 natural are ");
+        int i;
+        for(i=1;i<=10;i++){
+            System.out.println(+i);
+        }
+    }
+    
+}

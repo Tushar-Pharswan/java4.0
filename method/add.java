681053a08b53162b7eb66e51560413bc5f119e25
Tushar-Pharswan <119163815+Tushar-Pharswan@users.noreply.github.com>
2023-05-23 08:30:50 +0000

    doner

--- method/add.java
+++ method/add.java
@@ -3,17 +3,18 @@ package method;
 import java.util.Scanner;
 
 public class add {
-    public static int sum(int a, int b){
-        Scanner a=new Scanner(System.in);
-        Scanner b=new Scanner(System.in);
-        int no1=a.nextInt();
-        int no2=b.nextInt();
-        int sum = no1+no2;
-        System.out.println(sum);
-
+    static Scanner sc=new Scanner(System.in);
+    public static int sum (){
+        System.out.println("Enter first no.");
+        int a=sc.nextInt();
+        System.out.println("Enter second no.");
+        int b=sc.nextInt();
+        int sum = a+b;
+        System.out.println("Sum is ");
+        System.out.println(a+ "+" +b +" = " + sum) ;
+        return sum;
     }
     public static void main(String[] args) {
-        sum;
+        sum();
     }
-    
 }

6ccf99d1179588af5926744f7e597729f1631c9e
Tushar-Pharswan <119163815+Tushar-Pharswan@users.noreply.github.com>
2023-05-23 08:03:45 +0000

    ss

---
+++ method/add.java
@@ -0,0 +1,19 @@
+package method;
+
+import java.util.Scanner;
+
+public class add {
+    public static int sum(int a, int b){
+        Scanner a=new Scanner(System.in);
+        Scanner b=new Scanner(System.in);
+        int no1=a.nextInt();
+        int no2=b.nextInt();
+        int sum = no1+no2;
+        System.out.println(sum);
+
+    }
+    public static void main(String[] args) {
+        sum;
+    }
+    
+}

34d092494037aeea1874d49055960c3ba3fbf551
Tushar-Pharswan <119163815+Tushar-Pharswan@users.noreply.github.com>
2023-05-31 11:45:05 +0000

    commit

---
+++ oops/calsses/game.java
@@ -0,0 +1,50 @@
+package oops.calsses;
+
+public class game {
+    void run() {
+
+    }
+    void punch(){
+
+    }
+    void shoot(){
+        System.out.println("shooter ");
+
+    }
+
+    static class amit extends game{
+        void drive(){
+            System.out.println("Best driver ");
+        }
+        void pilot(){
+            System.out.println("pro pilot ");
+        }
+        void shoot (){
+            System.out.println(" Amit is pro shooter ");
+        }
+    }
+    static class chiki extends game{
+        void pilot(){
+            System.out.println("avg pilot ");
+        }
+        void shoot(){
+            System.out.println("chikki is a noob shooter ");
+        }   
+    }
+    static class tushar extends game{
+        void planer(){
+            System.out.println("heist planner ");
+        }
+    }
+    public static void main(String[] args) {
+        amit a1=new amit();
+        chiki c1=new chiki();
+        tushar t1=new tushar();
+        a1.drive();
+        a1.pilot();
+        c1.pilot();
+        t1.shoot();
+        
+    }
+}
+

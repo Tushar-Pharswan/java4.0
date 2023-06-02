fcf7dee230f7186cf7b02a1b5e1f663b424cb6c4
Tushar-Pharswan <pharswantushar@gmail.com>
2023-06-02 16:54:56 +0530

    asdfadsf

---
+++ collection_framework/arraylist.java
@@ -0,0 +1,11 @@
+package collection_framework;
+
+import java.util.ArrayList;
+
+public class arraylist {
+    ArrayList <Integer> arr=new ArrayList<>();
+    arr.add = 2;
+    public static void main(String[] args) {
+        arr;
+    }
+}

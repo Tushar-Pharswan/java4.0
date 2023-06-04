9bc318bc161958889cb1efecffa6c34f59e9d052
Tushar-Pharswan <pharswantushar@gmail.com>
2023-06-04 12:32:32 +0530

    rt

---
+++ DSA/Array/reverse.java
@@ -0,0 +1,10 @@
+package DSA.Array;
+
+public class reverse {
+    public static void main(String[] args) {
+        int arr[]=new int[]{1,2,3,4};
+        System.out.println("array before " +arr);
+       }
+       
+       
+}
---
+++ DSA/searching/binary.java
@@ -0,0 +1,5 @@
+package DSA.searching;
+
+public class binary {
+    
+}

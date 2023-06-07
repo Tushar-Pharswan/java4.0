b65c5dbd0994214c6dd37d91fc2de2dcdf33cbb1
Tushar-Pharswan <pharswantushar@gmail.com>
2023-06-07 19:35:40 +0530

    sfgh

--- DSA/recursion/array_aabbcc.java
+++ DSA/recursion/array_aabbcc.java
@@ -2,8 +2,15 @@ package DSA.recursion;
 
 public class array_aabbcc {
     public static void main(String[] args) {
-        char arr []=new char []{'a','a','b','b','c','c'};
-        
+        char arr[] = new char[] { 'a', 'a', 'b', 'b', 'c', 'c' };
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] == arr[i + 1]) {
+                arr[i + 1] = '#';
+            }
+        }
+        String copString = String.copyValueOf(arr);
+        System.out.println(copString);
+
     }
-    
+
 }

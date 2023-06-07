9d5805a51b6584d952e942ec17b8832fc4897899
Tushar-Pharswan <pharswantushar@gmail.com>
2023-06-07 18:54:51 +0530

    one mor down

--- DSA/recursion/ocurencesIndexToNewArr.java
+++ DSA/recursion/ocurencesIndexToNewArr.java
@@ -1,6 +1,21 @@
 package DSA.recursion;
 
+import java.util.ArrayList;
+
 public class ocurencesIndexToNewArr {
-    int arr[]=new int[]{1,2,1,3,1,2,5};
-     
+    public static void main(String[] args) {
+        int arr[] = new int[] { 1, 2, 1, 3, 1, 2, 5 };
+        ArrayList<Integer> arr2 = new ArrayList<>();
+        
+        int i = arr.length - 1;
+        
+        while (i >= 0) {
+            if (arr[i] == 1) {
+                arr2.add(i);
+            }
+            i--;
+        }
+        System.out.println(arr2);
+    }
+
 }

18ed233deabb5aa5699078165c8580b48af84ffe
Tushar-Pharswan <pharswantushar@gmail.com>
2023-06-07 19:53:10 +0530

    sdf

--- DSA/recursion/power.java
+++ DSA/recursion/power.java
@@ -1,6 +1,18 @@
 package DSA.recursion;
 
 public class power {
-    
+
+    //loop 
+    static int pow(int num,int num2){
+        int ans=1;
+
+        for (int i=0;i<num2;i++){
+            ans=ans*num;
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        System.out.println(pow(2, 5));
+    }
     
 }

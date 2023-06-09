3897e8d0a90a07766af3d6aed3d281d3df85408f
Tushar-Pharswan <pharswantushar@gmail.com>
2023-06-09 17:09:01 +0530

    sdfg

--- DSA/recursion/ArrayFromLast.java
+++ DSA/recursion/ArrayFromLast.java
@@ -1,15 +1,29 @@
 package DSA.recursion;
 
 public class ArrayFromLast {
-    public static void main(String[] args) {
-        int arr[]=new int[]{1,2,3,4};
-        int size = arr.length-1;
-        while(size>0){
-            if (arr[size]==2){
-                System.out.println(" 2 is at index "+size);
-            }
-            size--;
+    static int rec(int arr[], int i, int key){
+        if(i == 0){
+            System.out.println("not present ");
+            return -1;
         }
+        if(arr[i] == key){
+            return i;
+        }
+
+        return rec(arr, --i, key);
+    }
+    public static void main(String[] args) {
+        int arr[]=new int[]{1,2,3,4,54,6,6,75,45};
+        int size = arr.length-1;
+        // while(size>0){
+        //     if (arr[size]==2){
+        //         System.out.println(" by loop 2 is at index "+size);
+        //     }
+        //     size--;
+        // }
+        
+
+        System.out.println("by rec : "+rec(arr, size, 54));
     }
     
 }
--- DSA/recursion/arrayOcurenceReplace.java
+++ DSA/recursion/arrayOcurenceReplace.java
@@ -3,19 +3,38 @@ package DSA.recursion;
 import java.util.Arrays;
 
 public class arrayOcurenceReplace {
+    // by recursion
+    static int[] rec(int arr1[], int j, int key) {
+        if (j == -1) {
+            System.out.println(" not presnt ");
+            return arr1;
+        }
+        if (arr1[j] == key) {
+            arr1[j] = 5;
+        }
+
+        return rec(arr1, --j, key);
+    }
+
+    // by loop
+    // static int[] loop(int arr[]) {
+    //     int i = arr.length - 1;
+    //     while (i >= 0) {
+    //         if (arr[i] == 1) {
+    //             arr[i] = 5;
+    //         }
+    //         i--;
+    //     }
+    //     System.out.println(Arrays.toString(arr));
+    //     return arr;
+    // }
+
     public static void main(String[] args) {
         int arr[] = new int[] { 1, 2, 1, 3, 1, 2, 5 };
-        
-        int i = arr.length - 1;
-        
-        while (i >= 0) {
-            if (arr[i] == 1) {
-                arr[i]=5;
-                
-            }
-            i--;
-        }
-        System.out.println(Arrays.toString(arr));
+        // int size=arr.length;
+        // System.out.println(loop(arr));
+        // loop(arr);
+       System.out.println( Arrays.toString(rec(arr, arr.length-1, 1)));
     }
-    
+
 }

06b4df34d655828f4790992b65c0635919a6a711
Tushar-Pharswan <pharswantushar@gmail.com>
2023-06-09 15:07:41 +0530

    asd

--- DSA/recursion/Armstrong.java
+++ DSA/recursion/Armstrong.java
@@ -2,18 +2,48 @@ package DSA.recursion;
 
 public class Armstrong {
 
-    //loop
+    // recursion
 
-    static double no1(int digit){
-        double sum = 0;
-        while ( 0<digit){
-            sum =sum + (Math.pow(digit%10,3 ));
-            digit=digit/10;
+    static int no2(int digit2, int sum2) {
+        if (digit2 == 0) {
+            return sum2;
+        }
+        int d=digit2%10;
+        sum2 = sum2 + (d*d*d);
+        return no2(--digit2/10, sum2);
+    }
+
+    // loop while
+
+    static int no1(int digit) {
+        int sum = 0;
+        while (0 < digit) {
+            int d = digit%10;
+            sum = sum + (d*d*d);
+            digit = digit / 10;
         }
         return sum;
     }
-    public static void main(String[] args) {
-        System.out.println("By loop "+no1(153));
+
+    // loop again for
+
+    static int no3(int digit3){
+        int sum3 = 0;
+        int count =digit3;
+        for(int i=0;i<=count;i++){
+            int d=digit3%10;
+            sum3=sum3+(d*d*d);
+            digit3=digit3/10;
+
+        }
+        return sum3;
     }
-    
+
+    public static void main(String[] args) {
+        System.out.println("By while loop " + no1(153));
+        System.out.println("By for loop " +no3(153));
+        System.out.println("By recursion " +no2(153, 0));
+        
+    }
+
 }
--- DSA/recursion/prime.java
+++ DSA/recursion/prime.java
@@ -1,12 +1,44 @@
 package DSA.recursion;
 
 public class prime {
-    static int number(int a) {
+    // recursion
 
+    static void number2(int num, int i) {
+        if(i==num){
+            //System.out.println("number is prime ");
+            System.out.print(num+" ");
+            return;
+        }
+        if(num%i == 0){
+            //System.out.println("number is not prime ");
+            return ;
+        }
+
+         number2(num, ++i);
+    }
+    // loop
+
+    static void number(int num) {
+        boolean check = true;
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                check = false;
+                break;
+            }
+
+        }
+        if (check == true) {
+            System.out.println(num + " is a prime no.");
+        } else {
+            System.out.println(num + " is not prime no.");
+        }
     }
 
     public static void main(String[] args) {
         number(10);
+        //number2(17, 2);
+        for(int i =2; i<500; i++){
+            number2(i, 2);
+        }
     }
-
 }

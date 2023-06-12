ba6353077f29789a1f67686b01f99a576ab50df3
Tushar-Pharswan <pharswantushar@gmail.com>
2023-06-12 14:48:59 +0530

    sdfgh

--- DSA/recursion/palindrome.java
+++ DSA/recursion/palindrome.java
@@ -3,6 +3,28 @@ package DSA.recursion;
 import string.char_charAt_int_index;
 
 public class palindrome {
+
+    //recursion
+
+    static String pali2(String s3,String s4,int size1,int size2){
+        
+        if(size1==s3.length()){
+            System.out.println("By Recursion \n");
+            if(s3.equals(s4))
+            System.out.println("its palindrom ");
+            else{
+                System.out.println("not palindrome");
+            }
+            return s3;
+        }
+        size2=s3.length();
+        
+        s4 = s4 + s3.charAt(size2 - 1);
+
+        return pali2(s3, s4, size1--,size2);
+    }
+
+    // loop 
     static String pali() {
         String s1 = new String("abba");
         String s2 = new String("");
@@ -23,6 +45,9 @@ public class palindrome {
 
     public static void main(String[] args) {
         System.out.println(pali());
+        String s3 = new String("abba");
+        String s4 = new String("");
+        pali2(s3, s4, 0,0);
 
     }
 }

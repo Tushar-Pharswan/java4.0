db4dfca8ed2e9fceb2833c064ee73c41dd692022
Tushar-Pharswan <pharswantushar@gmail.com>
2023-06-14 18:02:06 +0530

    z

---
+++ DSA/recursion/smallSMALL.java
@@ -0,0 +1,22 @@
+package DSA.recursion;
+
+public class smallSMALL {
+
+    // static char[] convert() {
+    //     char charmessage[] = new char[] { 'a','b' };
+    //     int counter = 0;
+    //     for(int i=0;i<charmessage.length-1;i++){
+    //     charmessage
+    // }
+
+    
+
+    public static void main(String[] args) {
+
+        //System.out.println(convert());
+
+        String a = "abcbcGGh";
+        System.out.println(a.toUpperCase());
+    }
+
+}

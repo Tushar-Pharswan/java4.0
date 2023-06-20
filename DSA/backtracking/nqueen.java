8e398d94c42a068d1220bb7b7a2c336d61bb6d14
Tushar-Pharswan <pharswantushar@gmail.com>
2023-06-20 15:57:31 +0530

    fgh

---
+++ DSA/backtracking/nqueen.java
@@ -0,0 +1,16 @@
+package DSA.backtracking;
+
+public class nqueen {
+    static void queen(boolean board[][],int row ,int column){
+
+    }
+    
+    public static void main(String[] args) {
+        boolean board[][] = new boolean[][] {
+                {false,false,false,false},
+                {false,false,false,false},
+                {false,false,false,false},
+                {false,false,false,false}
+        };
+    }
+}

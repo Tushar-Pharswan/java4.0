83ecc9955adb03173a8467f819c9f51c00b64c55
Tushar-Pharswan <pharswantushar@gmail.com>
2023-06-21 15:14:00 +0530

    dfgh

--- DSA/backtracking/nqueen.java
+++ DSA/backtracking/nqueen.java
@@ -1,16 +1,60 @@
 package DSA.backtracking;
 
+import java.util.Arrays;
+
 public class nqueen {
-    static void queen(boolean board[][],int row ,int column){
+
+    static boolean isSafe(boolean board[][], int row, int col) {
+
+        for (int i = row; i >= 0; i--) {
+            if (board[i][col] == true) {
+                return false;
+            }
+        }
+
+        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == true) {
+                return false;
+            }
+        }
+
+        for (int i = row, j = col; i >= 0 && j < board[row].length; i--, j++) {
+            if (board[i][j] == true) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    static void queen(boolean board[][], int row) {
+
+        if (row == board.length) {
+            for (boolean[] arr : board) {
+                System.out.println(Arrays.toString(arr));
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int col = 0; col < board[row].length; col++) {
+            if (isSafe(board, row, col) == true) {
+                board[row][col] = true;
+                queen(board, row + 1);
+                board[row][col] = false;
+            }
+        }
 
     }
-    
+
     public static void main(String[] args) {
-        boolean board[][] = new boolean[][] {
-                {false,false,false,false},
-                {false,false,false,false},
-                {false,false,false,false},
-                {false,false,false,false}
+        boolean board[][] = {
+                { false, false, false, false },
+                { false, false, false, false },
+                { false, false, false, false },
+                { false, false, false, false }
         };
+
+        queen(board, 0);
     }
 }

2114330aa700a6f2dccd366a533fc6a322a496ec
Tushar-Pharswan <pharswantushar@gmail.com>
2023-06-30 00:55:03 +0530

    sfghjk

--- DSA/backtracking/rough.java
+++ DSA/backtracking/rough.java
@@ -2,15 +2,15 @@ package DSA.backtracking;
 
 public class rough {
     static void rat(int maze[][], int right, int down, String ans) {
-        if (right == maze.length - 1 && down == maze.length-1) {
+        if (right == maze.length - 1 && down == maze.length - 1) {
             System.out.println(ans);
             return;
         }
-        if(right<maze.length-1){
-        rat(maze, right + 1, down, ans+'R');
+        if (right < maze.length - 1) {
+            rat(maze, right + 1, down, ans + 'R');
         }
-        if(down<maze.length-1){
-        rat(maze, right, down+1, ans+'D');
+        if (down < maze.length - 1) {
+            rat(maze, right, down + 1, ans + 'D');
         }
     }
 

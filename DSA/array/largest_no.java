c25c89f17f34dfd3ce6aab3c10c03f0b3db4e983
Tushar-Pharswan <pharswantushar@gmail.com>
2023-06-30 13:55:02 +0530

    cvbn

---
+++ DSA/array/largest_no.java
@@ -0,0 +1,38 @@
+package DSA.array;
+
+public class largest_no {
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 3, 8, 4, 9, 3, 5 };
+        int max = arr[0];
+        int secondMax = arr[0];
+        int thirdMax = arr[0];
+
+        for (int i = 0; i < arr.length; i++) {
+            if (max < arr[i]) {
+                max = arr[i];
+            }
+
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != max) {
+                if (secondMax < arr[i]) {
+                    secondMax = arr[i];
+                }
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != secondMax) {
+                if (thirdMax < arr[i]) {
+                    thirdMax = arr[i];
+                }
+            }
+        }
+
+        System.out.println(max);
+        System.out.println(secondMax);
+        System.out.println(thirdMax);
+
+    }
+
+}

648dabb61bcbdf3dacd0096e8519f51433760c25
Tushar-Pharswan <pharswantushar@gmail.com>
2023-07-10 13:42:04 +0530

    asdf

--- DSA/array/arraysortedYesNo.java
+++ DSA/array/arraysortedYesNo.java
@@ -2,7 +2,7 @@ package DSA.array;
 
 public class arraysortedYesNo {
     static void sorted(int arr[]){
-        for(int i=0 ; i<arr.length-1;i++){
+        for(int i=0 ; i<arr.length-1 ;i++){
             if (arr[i]<=arr[i+1]){
                 System.out.println("yes");
             }

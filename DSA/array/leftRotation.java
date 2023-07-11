a5c0edf12f29627dc1c91c59dc93a8cb13423047
Tushar-Pharswan <pharswantushar@gmail.com>
2023-07-11 16:55:18 +0530

    asdf

--- DSA/array/leftRotation.java
+++ DSA/array/leftRotation.java
@@ -1,15 +1,20 @@
 package DSA.array;
 
+import java.util.Arrays;
+
 public class leftRotation {
     public static void main(String[] args) {
         int arr[]={1,2,3,4,5};
         int n =arr.length-1;
+        int temp=arr[0];
+        System.out.println(temp);
         for(int i=0;i<n;i++){
-            int temp=arr[0];
             arr[i]=arr[i+1];
-            arr[n]=temp;
+            if(i == arr.length){
+                arr[i] = temp;
+            }
         }
-    
+        System.out.println(Arrays.toString(arr));
     }
     
 }

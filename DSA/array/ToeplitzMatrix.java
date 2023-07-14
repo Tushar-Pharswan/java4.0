9f73f05a04cfcb06ff9422188ca0ce44ba70ab9d
Tushar-Pharswan <pharswantushar@gmail.com>
2023-07-14 14:07:42 +0530

    yf

--- DSA/array/ToeplitzMatrix.java
+++ DSA/array/ToeplitzMatrix.java
@@ -2,6 +2,11 @@ package DSA.array;
 
 public class ToeplitzMatrix {
     public static void main(String[] args) {
+        int matrix[][]={
+            {1,2,3,4},
+            {5,1,2,3},
+            {4,5,1,2}
+        };
         
     }
 }

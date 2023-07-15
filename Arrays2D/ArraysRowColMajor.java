a255a5e7abeb5769a14aeecec5dfc6b1f65a14ea
Tushar-Pharswan <pharswantushar@gmail.com>
2023-07-15 17:10:31 +0530

    Transpose

---
+++ Arrays2D/ArraysRowColMajor.java
@@ -0,0 +1,27 @@
+package Arrays2D;
+
+public class ArraysRowColMajor {
+    public static void main(String[] args) {
+        int x[][] = {
+                { 1, 2, 3 },
+                { 5, 6, 7 },
+                { 7, 8, 9 }
+        };
+        for (int i = 0; i < x.length; i++) {
+            for (int j = 0; j < x[i].length; j++) {
+                System.out.print(x[i][j] + ",");
+            }
+            System.out.println();
+
+        }
+        System.out.println("=================");
+        // Acess data in column major
+        for (int i = 0; i < x.length; i++) {
+            for (int j = 0; j < x[i].length; j++) {
+                System.out.print(x[j][i] + ",");
+            }
+            System.out.println();
+
+        }
+    }
+}

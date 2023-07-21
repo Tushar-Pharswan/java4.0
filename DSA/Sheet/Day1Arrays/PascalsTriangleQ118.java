06ec2fcc3ec118f058b3c7b93b620443e3714fbd
Tushar-Pharswan <pharswantushar@gmail.com>
2023-07-21 15:46:19 +0530

    variation 2

--- DSA/Sheet/Day1Arrays/PascalsTriangleQ118.java
+++ DSA/Sheet/Day1Arrays/PascalsTriangleQ118.java
@@ -2,9 +2,37 @@ package DSA.Sheet.Day1Arrays;
 
 public class PascalsTriangleQ118 {
 
-    /* variation 1 
-    In this case, we are given the row number r and the column number c, and we need to find out the element at position (r,c). 
-    */
+    // variation 2
+
+    //Given the row number n. Print the n-th row of Pascal’s triangle.
+
+    static void pascalTriangle(int n){
+        long res = 1;
+        System.out.println(res); 
+        //rest of the no.
+
+        for(int j = 1;j<n;j++){
+            res = res * (n-j);
+            res = res/j;
+            System.out.print (res + " ");
+        }
+        System.out.println();
+
+        public static void main(String[] args) {
+            int n = 5;
+            pascalTriangle(n);
+        }
+    }
+
+
+
+
+    /* 
+
+    // variation 1
+
+    //In this case, we are given the row number r and the column number c, and we need to find out the element at position (r,c). 
+    
 
     public static long ncr(int n , int r){
         long res =1;
@@ -25,4 +53,6 @@ public class PascalsTriangleQ118 {
         long element = pascalTriangle(r, c);
         System.out.println("The element at position (r,c) is: " + element);
     }
+
+    */
 }

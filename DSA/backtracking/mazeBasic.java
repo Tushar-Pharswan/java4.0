0dab1dd79ac326840c80ff9b854c1c6fcc260d78
Tushar-Pharswan <pharswantushar@gmail.com>
2023-08-16 14:44:11 +0530

    asdf

--- DSA/backtracking/mazeBasic.java
+++ DSA/backtracking/mazeBasic.java
@@ -3,14 +3,14 @@ package DSA.backtracking;
 public class mazeBasic {
     static void rat(int maze[][], int right, int down, String ans) {
         if (right == maze.length - 1 && down == maze.length-1) {
-            System.out.println(ans+ans);
+            System.out.println(ans);
             return;
         }
         if(right<maze.length-1){
-        rat(maze, right + 1, down, ans);
+        rat(maze, right + 1, down, ans+"R");
         }
         if(down<maze.length-1){
-        rat(maze, right, down+1, ans);
+        rat(maze, right, down+1, ans+"D");
         }
     }
 

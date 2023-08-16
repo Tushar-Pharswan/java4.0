4005b6db31a1ce6db3552a2e9f1e913996c06df0
Tushar-Pharswan <pharswantushar@gmail.com>
2023-08-16 12:45:46 +0530

    df

--- DSA/Sheet/Day3Arrays/ss.java
+++ DSA/Sheet/Day3Arrays/ss.java
@@ -1,16 +1,21 @@
-package DSA.backtracking;
+package DSA.Sheet.Day3Arrays;
 
-public class mazeBasic {
+class ss {
     static void rat(int maze[][], int right, int down, String ans) {
+
+        int count =0;
         if (right == maze.length - 1 && down == maze.length-1) {
-            System.out.println(ans+ans);
+            
+            System.out.println(count);
             return;
         }
         if(right<maze.length-1){
-        rat(maze, right + 1, down, ans);
+            count++;
+        rat(maze, right + 1, down, ans+'R');
         }
         if(down<maze.length-1){
-        rat(maze, right, down+1, ans);
+            count++;
+        rat(maze, right, down+1, ans+'D');
         }
     }
 
--- DSA/backtracking/rough.java
+++ DSA/backtracking/rough.java
@@ -1,17 +1,23 @@
 package DSA.backtracking;
 
 public class rough {
-    static void rat(int maze[][], int right, int down, String ans) {
+    static void rat(int maze[][], int right, int down, int ans) {
+        
         if (right == maze.length - 1 && down == maze.length - 1) {
-            System.out.println(ans);
+            System.out.println(count);
             return;
         }
+        int count=0;
+        for (int i=0 ; i<maze.length;i++){
         if (right < maze.length - 1) {
-            rat(maze, right + 1, down, ans + 'R');
+            rat(maze, right + 1, down, ans);
         }
         if (down < maze.length - 1) {
-            rat(maze, right, down + 1, ans + 'D');
+            rat(maze, right, down + 1, ans);
         }
+
+        count++;
+    }
     }
 
     public static void main(String[] args) {
@@ -20,6 +26,6 @@ public class rough {
                 { 0, 0, 0 },
                 { 0, 0, 0 },
         };
-        rat(maze, 0, 0, "");
+        rat(maze, 0, 0, 0);
     }
 }

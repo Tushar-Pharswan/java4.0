ee963a3529414a590a1a8bcb0d45d45b47c3438c
Tushar-Pharswan <pharswantushar@gmail.com>
2023-08-21 14:34:12 +0530

    asd

--- collection_framework/LinkedList2.java
+++ collection_framework/LinkedList2.java
@@ -1,5 +1,51 @@
 package collection_framework;
 
+import java.util.List;
+
 public class LinkedList2 {
-    
+    Node head;
+
+    class Node {
+        String data;
+        Node next;
+
+        Node(String data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    // add - first
+
+    public void addFirst(String data) {
+        Node newNode = new Node(data); 
+        if (head == null) {
+            head = newNode;
+            return;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+        
+    }
+
+    public void addLast(String data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node useHead = head;
+            while ( useHead.next !=null){
+                useHead=useHead.next;
+            }
+            useHead.next=newNode;
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkedList2 list = new LinkedList2();
+        list.addFirst("a");
+        list.addFirst("is");
+        
+    }
 }

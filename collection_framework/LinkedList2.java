a4fbfa9093b43b03e2c8db84eea7a5773a08522a
Tushar-Pharswan <pharswantushar@gmail.com>
2023-08-22 21:44:24 +0530

    y

--- collection_framework/LinkedList2.java
+++ collection_framework/LinkedList2.java
@@ -1,9 +1,13 @@
 package collection_framework;
 
-import java.util.List;
-
 public class LinkedList2 {
     Node head;
+
+    private int size;
+
+    LinkedList2() {
+        this.size = 0;
+    }
 
     class Node {
         String data;
@@ -12,6 +16,7 @@ public class LinkedList2 {
         Node(String data) {
             this.data = data;
             this.next = null;
+            size++;
         }
     }
 
@@ -35,33 +40,95 @@ public class LinkedList2 {
             head = newNode;
         } else {
             Node useHead = head;
-            while ( useHead.next !=null){
-                useHead=useHead.next;
-            }
-            useHead.next=newNode;
-        }
-    }
-    //Print
-        public void printing(){
-
-            if (head == null){
-                System.out.println( " List is empty ");
-                return;
-            }
-            Node useHead = head;
-            while(useHead.next !=null){
-                System.out.print(useHead.data+ " -> ");
+            while (useHead.next != null) {
                 useHead = useHead.next;
             }
-
-            System.out.println( "NULL");
+            useHead.next = newNode;
         }
+    }
+
+    // delete first
+
+    public void deleteFirst() {
+        if (head == null) {
+            System.out.println("This list is empty");
+            return;
+        }
+
+        size--;
+        head = head.next;
+    }
+
+    // delete last
+
+    public void deleteLast() {
+        if (head == null) {
+            System.out.println("Yhe list is empty");
+            return;
+        }
+
+        size--;
+        // side case 0.1
+        if (head.next == null) {
+            head = null;
+            return;
+        }
+
+        Node secondLast = head;
+        Node lastNode = head.next;// head.next = null -> lastNode =null
+
+        while (lastNode.next != null) {// null.next #error for error not to occur we make side case up 0.1
+            lastNode = lastNode.next;
+            secondLast = secondLast.next;
+        }
+
+        secondLast.next = null;
+    }
+
+    // Print
+    public void printing() {
+
+        if (head == null) {
+            System.out.println(" List is empty ");
+            return;
+        }
+        Node useHead = head;
+        while (useHead != null) {
+            System.out.print(useHead.data + " -> ");
+            useHead = useHead.next;
+        }
+
+        System.out.println("NULL");
+    }
+
+    public int getSize() {
+        return size;
+    }
 
     public static void main(String[] args) {
         LinkedList2 list = new LinkedList2();
-        list.addLast("a");
-        list.addLast("is");
-        list.addLast("the");
+        list.addFirst("a");
+        list.addFirst("is");
         list.printing();
+
+        list.addLast("List");
+        list.printing();
+
+        list.addFirst("This");
+        list.printing();
+
+        list.deleteFirst();
+        list.printing();
+
+        list.deleteLast();
+        list.printing();
+
+        System.out.println(list.getSize());
+
+        list.addFirst("This");
+        list.printing();
+
+        System.out.println(list.getSize());
+        
     }
 }
--- collection_framework/Linked_list.java
+++ collection_framework/Linked_list.java
@@ -10,7 +10,12 @@ public class Linked_list {
         linking.add("Tushar ");
         linking.add("Pharswan");
 
+        linking.addLast("is your name");
+        linking.add("withoutaddLast");
+
         System.out.println(linking);
+
+        System.out.println(linking.size());
         
         Iterator<String> it = linking.descendingIterator ();
         while(it.hasNext()){
